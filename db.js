f188ad35a96ac03f7431c6428c6fb495bfdba4c7
Alexandre Moreira <brainmunchers@gmail.com>
2021-09-01 09:55:59 +0200

    add initial structure

---
+++ db.js
@@ -0,0 +1,37 @@
+const bowls = [
+  {
+    title: 'Vegan Miso Ramen',
+    author: 'Alex',
+    image: 'vegan-miso',
+    rating: 0,
+  },
+  {
+    title: 'Vegan Tonkotsu Ramem',
+    author: 'Paul',
+    image: 'vegan-tonkotsu',
+    rating: 0,
+  },
+];
+
+const categories = [
+  {
+    title: 'creamy',
+    image: 'creamy-category',
+    id: 1,
+  },
+  {
+    title: 'spicy',
+    image: 'spicy-category',
+    id: 2,
+  },
+  {
+    title: 'mushrooms',
+    image: 'mushrooms-category',
+    id: 3,
+  },
+];
+
+module.exports = {
+  bowls,
+  categories,
+};
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('hello');
+const { ApolloServer, gql } = require('apollo-server');
+const { bowls, categories } = require('./db');
+
+const typeDefs = gql`
+  type Bowl {
+    title: String!
+    author: String!
+    image: String!
+    rating: Int!
+  }
+
+  type Category {
+    title: String!
+    image: String!
+    id: Int!
+  }
+
+  type Query {
+    bowls: [Bowl]
+    categories: [Category]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    bowls: () => bowls,
+    categories: () => categories,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
